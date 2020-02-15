f0b6ff1cd5a88cd2c5a0096560ab7e51419c965e
Nursultan <tashanov.98@mail.ru>
2020-02-15 16:49:53 +0500

    add src

---
+++ src/src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./App.css";
+import Am from "./Tashanov";
+
+const App = () => {
+  return (
+    <div>
+      <Am />
+      <ui>
+        <li>css</li>
+        <li>html</li>
+        <li>sass</li>
+        <li>js</li>
+      </ui>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/src/Tashanov.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+const Am = () => {
+  return <div>rfr</div>;
+};
+export default Am;
